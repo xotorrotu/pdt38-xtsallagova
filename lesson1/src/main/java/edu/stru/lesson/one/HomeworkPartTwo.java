8972d5c01a7e9eeaa0940afa38c117815722c8cb
unknown <C:\Users\csg\AppData\Roaming\The Bat!>
2017-02-16 02:19:35 +0400

    вместо функции distance добавлен метод в классе Point, в запускаемом классе добавлен пример использования этого метода

--- lesson1/src/main/java/edu/stru/lesson/one/HomeworkPartTwo.java
+++ lesson1/src/main/java/edu/stru/lesson/one/HomeworkPartTwo.java
@@ -7,17 +7,8 @@ public class HomeworkPartTwo {
         Point p1 = new Point(9.5,3);
         Point p2 = new Point(8,1);
 
-        System.out.println("Расстояние между точкой A("+p1.x+";"+p1.y+") и точкой B("+p2.x+";"+p2.y+")="+distance(p1,p2));
+        System.out.println("Расстояние между точками A("+p1.x+";"+p1.y+") и B("+p2.x+";"+p2.y+") = "+p1.distance(p2));
 	}
-
-
-    //функнция distance вычисляет расстояние между двумя точками
-    public static double distance(Point p1, Point p2){
-
-        return Math.sqrt(Math.pow(p1.x-p2.x, 2) + Math.pow(p1.y-p2.y, 2) );
-
-    }
-
 
 }
 
--- lesson1/src/main/java/edu/stru/lesson/one/Point.java
+++ lesson1/src/main/java/edu/stru/lesson/one/Point.java
@@ -14,6 +14,12 @@ public class Point {
     public Point(double x, double y){
         this.x=x;
         this.y=y;
+    }
+
+    //функнция distance вычисляет расстояние между двумя точками
+    public double distance(Point p){
+
+        return Math.sqrt(Math.pow(this.x-p.x, 2) + Math.pow(this.y-p.y, 2) );
 
     }
 
