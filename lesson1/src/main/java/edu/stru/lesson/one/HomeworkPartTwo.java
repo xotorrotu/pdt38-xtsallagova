4696122c3cef47a742b7b7020e6184eb46e342ba
unknown <C:\Users\csg\AppData\Roaming\The Bat!>
2017-02-16 01:59:21 +0400

    добавлен модуль lesson1 и в него добавлен класс Point для представления точек на двумерной плоскости

---
+++ lesson1/src/main/java/edu/stru/lesson/one/HomeworkPartTwo.java
@@ -0,0 +1,11 @@
+package edu.stru.lesson.one;
+
+public class HomeworkPartTwo {
+
+	public static void main (String[] args){
+        int num =1;
+        System.out.println("Homework "+num);
+	}
+
+}
+
---
+++ lesson1/src/main/java/edu/stru/lesson/one/Point.java
@@ -0,0 +1,20 @@
+package edu.stru.lesson.one;
+
+/**
+ * Created by ksu on 15.02.2017.
+ */
+public class Point {
+
+    //координаты точки
+    public double x;
+    public double y;
+
+
+    //конструктор
+    public Point(double x, double y){
+        this.x=x;
+        this.y=y;
+
+    }
+
+}
