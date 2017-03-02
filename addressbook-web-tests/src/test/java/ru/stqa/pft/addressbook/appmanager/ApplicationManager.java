75b4b2119f6f18c5268a8cac9f2f377053047cd5
unknown <C:\Users\csg\AppData\Roaming\The Bat!>
2017-03-02 22:19:58 +0400

    расщепление и специализация базового класса - лекция 2.10

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -0,0 +1,51 @@
+package ru.stqa.pft.addressbook.appmanager;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoAlertPresentException;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Created by ksu on 02.03.2017.
+ */
+public class ApplicationManager {
+
+    FirefoxDriver wd;
+
+    private SessionHelper sessionHelper;
+    private NavigationHelper navigationHelper;
+    private GroupHelper groupHelper;
+
+    public static boolean isAlertPresent(FirefoxDriver wd) {
+        try {
+            wd.switchTo().alert();
+            return true;
+        } catch (NoAlertPresentException e) {
+            return false;
+        }
+    }
+
+    public void init() {
+        wd = new FirefoxDriver();
+        wd.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
+        wd.get("http://localhost:8080/addressbook/");
+        navigationHelper = new NavigationHelper(wd);
+        groupHelper = new GroupHelper(wd);
+        sessionHelper = new SessionHelper(wd)
+        sessionHelper.login("admin", "secret");
+    }
+
+
+    public void stop() {
+        wd.quit();
+    }
+
+    public GroupHelper getGroupHelper() {
+        return groupHelper;
+    }
+
+    public NavigationHelper getNavigationHelper() {
+        return navigationHelper;
+    }
+}
---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/NavigationHelper.java
@@ -0,0 +1,19 @@
+package ru.stqa.pft.addressbook.appmanager;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+/**
+ * Created by ksu on 02.03.2017.
+ */
+public class NavigationHelper {
+    private FirefoxDriver wd;
+
+    public NavigationHelper(FirefoxDriver wd) {
+        this.wd = wd;
+    }
+
+    public void gotoGroupPage() {
+        wd.findElement(By.linkText("groups")).click();
+    }
+}
