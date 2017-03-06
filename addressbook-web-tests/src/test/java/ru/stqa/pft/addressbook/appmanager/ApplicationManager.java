7306f9949b7415b410c0cdaa4287034d76ab62ef
unknown <C:\Users\csg\AppData\Roaming\The Bat!>
2017-03-06 21:52:36 +0400

    7-ое задание, добавлена возможность запуска тестов в бразузерах Chrome и IE

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -1,23 +1,41 @@
 package ru.stqa.pft.addressbook.appmanager;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.BrowserType;
 
+import java.util.Objects;
 import java.util.concurrent.TimeUnit;
 
-/**
- * Created by ksu on 02.03.2017.
- */
+
 public class ApplicationManager {
 
-    FirefoxDriver wd;
+    WebDriver wd;
 
     private SessionHelper sessionHelper;
     private NavigationHelper navigationHelper;
     private GroupHelper groupHelper;
     private ContactsHelper contactsHelper;
+    private String browser;
+
+    public ApplicationManager(String browser) {
+        this.browser = browser;
+    }
 
     public void init() {
-        wd = new FirefoxDriver();
+
+        if (browser.equals(BrowserType.FIREFOX)) {
+            wd = new FirefoxDriver();
+        } else if (browser.equals(BrowserType.CHROME)){
+            wd = new ChromeDriver();
+        }else if (browser.equals(BrowserType.IE )){
+            wd = new InternetExplorerDriver();
+        }
+
+
+
         wd.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
         wd.get("http://localhost:8080/addressbook/");
         navigationHelper = new NavigationHelper(wd);
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ContactsHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ContactsHelper.java
@@ -1,6 +1,7 @@
 package ru.stqa.pft.addressbook.appmanager;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import ru.stqa.pft.addressbook.model.PersonDataEmails;
 import ru.stqa.pft.addressbook.model.PersonDataName;
@@ -12,7 +13,7 @@ import ru.stqa.pft.addressbook.model.PersonDataPhones;
 
 public class ContactsHelper extends HelperBase{
 
-    public ContactsHelper(FirefoxDriver wd) {
+    public ContactsHelper(WebDriver wd) {
         super(wd);
     }
 
